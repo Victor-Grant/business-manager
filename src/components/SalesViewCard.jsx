f33931355ee83b6c0054b9229bfb98138e5627c5
Victor Grant <grantvictor725@gmail.com>
2025-10-21 07:43:33 -0700

    final layout cleanup

---
+++ src/components/SalesViewCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SalesViewCard = () => {
+  return <div></div>;
+};
+
+export default SalesViewCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import TopBar from "../components/TopBar.jsx";
 
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  const [searchInput, setSearchInput] = useState("");
+  return (
+    <div style={styles.container}>
+      <TopBar pageTitle={`Dashboard`} setValue={setSearchInput} />
+      <div></div>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    height: "100vh",
+  },
 };
 
 export default Dashboard;
