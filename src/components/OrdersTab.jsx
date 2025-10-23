8d1b8fd17b313822d8eb6ff234624d2a9a14b094
Victor Grant <grantvictor725@gmail.com>
2025-10-23 09:14:05 -0700

    Added Sales List

--- src/components/OrdersTab.jsx
+++ src/components/OrdersTab.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 
 const OrdersTab = () => {
-  return <div></div>;
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <div style={styles.container}>
+      <div style={styles.calenderContainer}>
+        <div>
+          From <input style={styles.input} type="date" />
+        </div>
+        <div>
+          To
+          <input
+            style={styles.input}
+            type="date"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+      </div>
+      <div>
+        <button style={styles.button}>Print</button>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottom: "1px solid #ccc",
+  },
+  calenderContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+  },
+  button: {
+    padding: "15px 40px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    border: "none",
+    borderRadius: 6,
+    fontSize: 18,
+    cursor: "pointer",
+  },
+  input: {
+    fontSize: 15,
+    lineHeight: "normal",
+  },
 };
 
 export default OrdersTab;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TopBar from "../components/TopBar.jsx";
 import OrdersBox from "../components/OrdersBox.jsx";
 import OrdersTab from "../components/OrdersTab.jsx";
+import SalesList from "../components/SalesList.jsx";
 
 const Orders = () => {
   return (
@@ -13,6 +14,9 @@ const Orders = () => {
           <OrdersBox infoTitle="Total Orders" infoData="150 Orders" />
           <OrdersBox infoTitle="Completed Orders" infoData="120 Orders" />
           <OrdersBox infoTitle="Cancelled Orders" infoData="5 Orders" />
+        </div>
+        <div>
+          <SalesList startDate={""} endDate={""} />
         </div>
       </div>
     </div>
