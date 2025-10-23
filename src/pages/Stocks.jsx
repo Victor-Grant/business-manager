fced440c73a62cfc1564805e0b760f9bd9842a24
Victor Grant <grantvictor725@gmail.com>
2025-10-23 05:42:43 -0700

    initialized design of stock page, added Info Box and Stock List Component

--- src/pages/Stocks.jsx
+++ src/pages/Stocks.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import TopBar from "../components/TopBar.jsx";
+import InfoBox from "../components/InfoBox.jsx";
 
 const Stocks = () => {
-  return <div>Stocks Page</div>;
+  return (
+    <div>
+      <TopBar pageTitle="Stocks" />
+      <div>
+        <div style={styles.infoBox}>
+          <InfoBox infoTitle="Total Stocks" infoData="320 Items" />
+          <InfoBox infoTitle="All Products" infoData="20 Items" />
+        </div>
+        <hr style={{ width: "100%" }} />
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  infoBox: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+    padding: 20,
+  },
 };
 
 export default Stocks;
