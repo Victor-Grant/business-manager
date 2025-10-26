67d11c3074a58498db270c58d7af45741e0f163f
Victor Grant <grantvictor725@gmail.com>
2025-10-26 11:41:21 -0700

    terminate button for add sales modal

--- src/components/AddStockModal.jsx
+++ src/components/AddStockModal.jsx
@@ -16,7 +16,6 @@ const AddStockModal = () => {
       price: parseFloat(stockPrice),
       quantity: parseInt(stockQuantity, 10),
     };
-    //await openStockDatabase();
     await addStocksData(newStock);
     setStockName("");
     setStockDescription("");
@@ -32,7 +31,21 @@ const AddStockModal = () => {
       {isOpen && (
         <div style={styles.container}>
           <div style={styles.modal}>
-            <h2>Add New Stock</h2>
+            <div style={styles.modalHeader}>
+              <h2>Add New Stock</h2>
+              <button
+                style={styles.closeButton}
+                onClick={() => setIsOpen(false)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="white"
+                  viewBox="0 0 640 640"
+                >
+                  <path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z" />
+                </svg>
+              </button>
+            </div>
             <div>
               <label style={{ display: "block", marginBottom: 10 }}>
                 <p>Stock Name:</p>
@@ -122,6 +135,21 @@ const styles = {
     padding: 8,
     borderRadius: 8,
     border: "1px solid #ccc",
+  },
+  modalHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  closeButton: {
+    backgroundColor: "rgba(255, 0, 0, 0.5)",
+    borderRadius: "50%",
+    color: "white",
+    border: "none",
+    width: 25,
+    height: 25,
+    padding: 5,
+    cursor: "pointer",
   },
 };
 
