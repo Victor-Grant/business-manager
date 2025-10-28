c2b4883afc697f894411e64b4b971542bf4ae5e8
Victor Grant <grantvictor725@gmail.com>
2025-10-28 04:51:26 -0700

    fixed search modal error

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,21 +1,49 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getAllStocksData } from "../StocksDatabase.js";
 
 const SearchModal = () => {
   const [openSearchModal, setOpenSearchModal] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchObjects, setSearchObjects] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const getSearchObjects = async () => {
+      const _searchObjects = await getAllStocksData();
+      setOptions(_searchObjects);
+      setSearchObjects(_searchObjects);
+    };
+
+    console.log("Rendering");
+
+    getSearchObjects();
+  }, []);
 
   const selectItem = (item) => {
-    if (!selectedItems.includes(item)) {
-      setSelectedItems([...selectedItems, item]);
-    }
+    setSelectedItems([...selectedItems, item]);
+    setSearchQuery("");
+    setOpenSearchModal(false);
+    console.log(selectedItems);
   };
 
   const removeItem = (id) => {
     setSelectedItems(selectedItems.filter((item) => item.id !== id));
   };
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    setOptions(
+      searchObjects.filter((item) => String(item.name).includes(e.target.value))
+    );
+    if (e.target.value.length > 0) {
+      setOpenSearchModal(true);
+    } else {
+      setOpenSearchModal(false);
+    }
+  };
+
   return (
     <div>
       <div>
@@ -39,8 +67,25 @@ const SearchModal = () => {
           </div>
         ))}
       </div>
-      <input type="text" placeholder="Search..." style={styles.input} />
-      {/* Additional search functionalities can be added here */}
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => handleSearchChange(e)}
+        style={styles.input}
+      />
+      <div>
+        {openSearchModal &&
+          options.map((data) => (
+            <button
+              onClick={() => selectItem(data)}
+              key={data.id}
+              style={styles.list}
+            >
+              {data?.name}
+            </button>
+          ))}
+      </div>
     </div>
   );
 };
@@ -66,6 +111,13 @@ const styles = {
     border: "none",
     borderRadius: 5,
     cursor: "pointer",
+  },
+  list: {
+    padding: 2,
+    lineHeight: "normal",
+    width: "100%",
+    border: "1px solid black",
+    borderRadius: "none",
   },
 };
 
